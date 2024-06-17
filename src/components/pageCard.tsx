dd961f497b0f19305d167e81533eac7806fbb04e
James McKenzie <j.mckenzie@adinstruments.com>
2024-06-18 09:50:57 +1200

    Fixed issues regarding scrollbar desync and removed borders for more white space

--- src/components/pageCard.tsx
+++ src/components/pageCard.tsx
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
 export const PageCard = styled.div`
-	padding-top: 5%;
-	padding-bottom: 5%;
-	width: 100%;
-	padding-left: 10%;
-	border-bottom: 1px solid #6fa587;
-	border-top: 1px solid #6fa587;
+  padding-top: 2%;
+  padding-bottom: 3%;
+  width: 100%;
+  padding-left: 10%;
 `;
 
 export const PageSection = styled.div``;
