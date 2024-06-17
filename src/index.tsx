d9680d8168feb5390f3747ec91b92cd70c8840ba
lunarised <lunarised@outlook.com>
2024-06-17 22:03:06 +1200

    Cleaned Up App root, removed unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,23 @@
 import React from "react";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { AboutMeSection } from "./pages/about-me";
-import { ExperienceSection } from "./pages/experience";
-import { HomeSection } from "./pages/home";
-import { ProjectsSection } from "./pages/projects";
-import { StyledButton } from "./components/ModalButton";
 import { LargeViewPort, SmallViewPort } from "./components/viewPort";
 import ReactDOM from "react-dom";
 import { Breakpoint, BreakpointProvider } from "react-socks";
 
-
 ReactDOM.render(
-  <BreakpointProvider>
-    <React.StrictMode>
-      <Breakpoint medium up>
-        <LargeViewPort />
-      </Breakpoint>
-      <Breakpoint small down>
-        <SmallViewPort />
-      </Breakpoint>
-    </React.StrictMode>
-  </BreakpointProvider>,
+	<BreakpointProvider>
+		<React.StrictMode>
+			<Breakpoint medium up>
+				<LargeViewPort />
+			</Breakpoint>
+			<Breakpoint small down>
+				<SmallViewPort />
+			</Breakpoint>
+		</React.StrictMode>
+	</BreakpointProvider>,
 
-  document.getElementById("root") as HTMLElement
+	document.getElementById("root") as HTMLElement
 );
 
 // If you want to start measuring performance in your app, pass a function
