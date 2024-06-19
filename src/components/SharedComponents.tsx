a304cb1a878491ce9d464e4b3d0350e0829b4ca3
James McKenzie <j.mckenzie@adinstruments.com>
2024-06-19 15:59:17 +1200

    Added UI Font for HTML icons

--- src/components/SharedComponents.tsx
+++ src/components/SharedComponents.tsx
@@ -2,71 +2,73 @@ import styled from "styled-components";
 import { SubjectTitle } from "./ContentStylings";
 
 export const CollapsableRow = (props: {
-	open: string;
-	setOpen: (open: string) => void;
-	name: string;
-	color: string;
-	children: React.ReactNode;
+  open: string;
+  setOpen: (open: string) => void;
+  name: string;
+  color: string;
+  children: React.ReactNode;
 }) => {
-	return (
-		<div style={{ paddingBottom: "4px" }}>
-			<ListItem
-				open={props.open}
-				setOpen={props.setOpen}
-				title={props.name}
-				color={props.color}
-			>
-				<CollapsableContent>{props.children}</CollapsableContent>
-			</ListItem>
-		</div>
-	);
+  return (
+    <div style={{ paddingBottom: "4px" }}>
+      <ListItem
+        open={props.open}
+        setOpen={props.setOpen}
+        title={props.name}
+        color={props.color}
+      >
+        <CollapsableContent>{props.children}</CollapsableContent>
+      </ListItem>
+    </div>
+  );
 };
 
 const CollapsableContent = styled.div`
-	padding: 3px;
-	margin-left: 10px;
+  padding: 3px;
+  margin-left: 10px;
 `;
 
 const DropdownHeader = styled.div`
-	display: flex;
-	column-gap: 5px;
-	padding-left: 5px;
-	color: #000000cc;
-	user-select: none;
-	cursor: pointer;
+  display: flex;
+  column-gap: 5px;
+  padding-left: 5px;
+  color: #000000cc;
+  user-select: none;
+  cursor: pointer;
 `;
 
 export const ListItem = (props: {
-	open: string;
-	setOpen: (open: string) => void;
-	title: string;
-	color: string;
-	children: React.ReactNode;
+  open: string;
+  setOpen: (open: string) => void;
+  title: string;
+  color: string;
+  children: React.ReactNode;
 }) => {
-	return (
-		<>
-			<DropdownHeader
-				onClick={() => {
-					props.open === props.title
-						? props.setOpen("none")
-						: props.setOpen(props.title);
-				}}
-			>
-				{props.open === props.title ? (
-					<span>&#11206;</span>
-				) : (
-					<span>&#11208;</span>
-				)}
-				<span style={{ color: props.color }}>&#9632;</span>
-				<SubjectTitle style={{ color: "#000000bb" }}>
-					{props.title}
-				</SubjectTitle>
-			</DropdownHeader>
-			{props.open === props.title ? (
-				<CollapsableContent>{props.children}</CollapsableContent>
-			) : (
-				<></>
-			)}
-		</>
-	);
+  return (
+    <>
+      <DropdownHeader
+        onClick={() => {
+          props.open === props.title
+            ? props.setOpen("none")
+            : props.setOpen(props.title);
+        }}
+      >
+        {props.open === props.title ? (
+          <span style={{ fontFamily: "UIFontRegular" }}>&#11206;</span>
+        ) : (
+          <span style={{ fontFamily: "UIFontRegular" }}>&#11208;</span>
+        )}
+        <span style={{ color: props.color, fontFamily: "UIFontRegular" }}>
+          &#9632;
+        </span>
+        <SubjectTitle style={{ color: "#000000bb" }}>
+          {props.title}
+        </SubjectTitle>
+      </DropdownHeader>
+      {props.open === props.title ? (
+        <CollapsableContent>{props.children}</CollapsableContent>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 };
