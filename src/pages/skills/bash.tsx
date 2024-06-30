2b0e6e1f2ad27f42106ab16af8d4e159c4db425a
lunarised <lunarised@outlook.com>
2024-06-30 23:06:08 +1200

    Forgot to style bash link

--- src/pages/skills/bash.tsx
+++ src/pages/skills/bash.tsx
@@ -1,5 +1,5 @@
 import { ContentParagraph } from "../../components/ContentStylings";
-import { CollapsibleRow } from "../../components/SharedComponents";
+import { CollapsibleRow, ContentLink } from "../../components/SharedComponents";
 import { SkillsProps } from "./skills";
 
 export const BashRow = (props: SkillsProps) => {
@@ -14,9 +14,9 @@ export const BashRow = (props: SkillsProps) => {
         I have a lot of experience with using Bash, as I use it as my preferred
         shell on my operating systems. While the Bash I write isn't technically
         impressive, it does what I need it to do, by automating tedious tasks{" "}
-        <a href="https://github.com/lunarised/jmake">
+        <ContentLink href="https://github.com/lunarised/jmake">
           such as automating Jeckyl metadata.
-        </a>
+        </ContentLink>
       </ContentParagraph>
     </CollapsibleRow>
   );
